914cf824c5b35efba3538a8de1715c356413d8a9
LongXiang <LongXiang@qq.com>
2022-12-26 11:28:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author lx
+ * @version 1.0
+ * @description: 测试
+ * @date 2022/12/26 11:17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
